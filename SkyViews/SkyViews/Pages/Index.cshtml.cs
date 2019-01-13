2254aa5f53161a7248889781a65663f6fb2957f2
Suraj <Suraj.Kalsi@sky.uk>
2019-01-13 16:46:36 +0000

    Added login functionality and bookings page information

--- SkyViews/SkyViews/Pages/Index.cshtml.cs
+++ SkyViews/SkyViews/Pages/Index.cshtml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -14,6 +16,8 @@ namespace SkyViews.Pages
 
         private readonly SkyViewsContext _context;
 
+        public String currentUser = null;
+
         public IndexModel(SkyViewsContext context)
         {
             _context = context;
@@ -21,10 +25,20 @@ namespace SkyViews.Pages
 
 
         public IList<Film> Film{ get; set; }
+        public object Membership { get; private set; }
+
         public async Task OnGetAsync()
         {
             Film = await _context.Film.ToListAsync();
+            getUser();
 
+        }
+
+        public void getUser()
+        {
+            WindowsIdentity user = WindowsIdentity.GetCurrent();
+            //currentUser = user.Name;
+            //currentUser = Membership.GetUser().UserName
         }
     }
 }
